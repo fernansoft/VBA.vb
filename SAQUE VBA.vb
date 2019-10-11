8a3cf7c18dc6269bb3897c3b79bf7eb905e24e46
fernansoft <54730133+fernansoft@users.noreply.github.com>
2019-10-11 18:01:45 -0300

    Update SAQUE VBA.vb

--- SAQUE VBA.vb
+++ SAQUE VBA.vb
@@ -1,42 +1,58 @@
-Dim n50, n20, n10, n05, n02 as Integer
-Dim saque as Integer
-InputBox("Isira o valor a ser sacado: ")
-While saque > 13
-	While saque > 53
-		saque = saque - 50
-		n50 = n50 + 1
-	Wend
-	While saque > 23
-		saque = saque - 20
-		n20 = n20 + 1
-	Wend
-	While saque > 13
-		saque = saque - 10
-		n10 = n10 + 1
-	Wend
-Wend
-If ((saque = 11) or (saque = 13)) Then
-	saque = saque - 5
-	n05 = n05 + 1
+Private Sub CommandButton1_Click()
+Dim n50, n20, n10, n05, n02 As Integer
+Dim saque As Integer
+n50 = 0
+n20 = 0
+n10 = 0
+n05 = 0
+n02 = 0
+saque = InputBox("Isira o valor a ser sacado: ")
+    If (saque = 50) Then
+        saque = saque - 50
+        n50 = n50 + 1
+    End If
+    If (saque = 20) Then
+        saque = saque - 20
+        n20 = n20 + 1
+    End If
+    If (saque = 10) Then
+        saque = saque - 10
+        n10 = n10 + 1
+    End If
+    While (saque > 53)
+        saque = saque - 50
+        n50 = n50 + 1
+    Wend
+    While (saque >= 23)
+        saque = saque - 20
+        n20 = n20 + 1
+    Wend
+    While (saque > 13)
+        saque = saque - 10
+        n10 = n10 + 1
+    Wend
+If ((saque = 11) Or (saque = 13)) Then
+    saque = saque - 5
+    n05 = n05 + 1
 End If
-If ((saque = 6) or (saque = 8)) Then
-	While saque >= 2
-		saque = saque - 2
-		n02 = n02 + 1
-	Wend
-	Else
-		While saque >= 10
-			saque = saque - 10
-			n10 = n10 + 1
-		Wend
-		While saque >= 5
-			saque = saque - 5
-			n05 = n05 + 1
-		Wend
-		While saque >= 2
-			saque = saque - 2
-			n05 = n02 + 1
-		Wend
+If ((saque = 6) Or (saque = 8)) Then
+    While saque >= 2
+        saque = saque - 2
+        n02 = n02 + 1
+    Wend
+    Else
+        While (saque >= 10)
+            saque = saque - 10
+            n10 = n10 + 1
+        Wend
+        While (saque >= 5)
+            saque = saque - 5
+            n05 = n05 + 1
+        Wend
+        While (saque >= 2)
+            saque = saque - 2
+            n05 = n02 + 1
+        Wend
 End If
-MsgBox("Você irá sacar:" & )
-
+MsgBox (("Você irá sacar:") & vbCrLf & n50 & (" notas de R$50,00") & vbCrLf & n20 & (" notas de R$20,00") & vbCrLf & n10 & (" notas de R$10,00") & vbCrLf & n05 & (" notas de R$05,00") & vbCrLf & n02 & (" notas de R$02,00"))
+End Sub
