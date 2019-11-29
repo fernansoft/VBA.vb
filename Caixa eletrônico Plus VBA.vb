958e54ad1e0b0e37615d48e869d3786b1a9f3b8b
fernansoft <54730133+fernansoft@users.noreply.github.com>
2019-11-29 17:37:27 -0300

    Update Caixa eletrônico Plus VBA.vb

--- Caixa eletrônico Plus VBA.vb
+++ Caixa eletrônico Plus VBA.vb
@@ -1,11 +1,38 @@
 Private Sub CaixaEletronico_Click()
-Dim menu As Integer
-fmenu (menu)
+Dim opcmenu As Integer
+Call fmenu
+Dim vnotasnocaixa(6) As Integer
 End Sub
-Function fmenu(menu)
-    menu = InputBox("Escolha uma opção para prosseguir:" & vbCrLf & "1 - Carregar notas" & vbCrLf & "2 - Retirar notas" & vbCrLf & "3 - Estatísticas" & vbCrLf & "9 - Fim" & vbCrLf & ">>> ")
-    While menu <> 1 And menu <> 2 And menu <> 3 And menu <> 9
-        MsgBox ("Não existe essa opção, favor selecionar uma das citadas no menu!")
-        menu = InputBox("Escolha uma opção para prosseguir:" & vbCrLf & "1 - Carregar notas" & vbCrLf & "2 - Retirar notas" & vbCrLf & "3 - Estatísticas" & vbCrLf & "9 - Fim" & vbCrLf & ">>> ")
+'Procedimento opcmenu principal (gerencia qual vai ser a função chamada)
+Sub fmenu()
+    opcmenu = InputBox("Escolha uma opção para prosseguir:" & vbCrLf & "1 - Carregar notas" & vbCrLf & "2 - Retirar notas" & vbCrLf & "3 - Estatísticas" & vbCrLf & "9 - Fim" & vbCrLf & ">>> ")
+    While opcmenu <> 1 And opcmenu <> 2 And opcmenu <> 3 And opcmenu <> 9
+        MsgBox ("Não existe essa opção, favor selecionar uma das citadas no opcmenu!")
+        opcmenu = InputBox("Escolha uma opção para prosseguir:" & vbCrLf & "1 - Carregar notas" & vbCrLf & "2 - Retirar notas" & vbCrLf & "3 - Estatísticas" & vbCrLf & "9 - Fim" & vbCrLf & ">>> ")
     Wend
-End Function
+    Select Case opcmenu
+        Case 1
+            Call fcarregarnotas
+        Case 2
+        Case 3
+        Case 9
+    End Select
+End Sub
+'Procedimento carregar notas insere por input quantas notas de cada cédula o caixa terá
+Sub fcarregarnotas()
+    Dim vnotasnocaixa(6) As Integer
+    vnotasnocaixa(0) = InputBox("Insira a quantidade de notas de R$100,00: ")
+    vnotasnocaixa(1) = InputBox("Insira a quantidade de notas de R$50,00: ")
+    vnotasnocaixa(2) = InputBox("Insira a quantidade de notas de R$20,00: ")
+    vnotasnocaixa(3) = InputBox("Insira a quantidade de notas de R$10,00: ")
+    vnotasnocaixa(4) = InputBox("Insira a quantidade de notas de R$05,00: ")
+    vnotasnocaixa(5) = InputBox("Insira a quantidade de notas de R$02,00: ")
+    ' vnotasnocaixa(1) = notas de R$100,00
+    ' vnotasnocaixa(2) = notas de R$50,00
+    ' vnotasnocaixa(3) = notas de R$20,00
+    ' vnotasnocaixa(4) = notas de R$10,00
+    ' vnotasnocaixa(5) = notas de R$05,00
+    ' vnotasnocaixa(6) = notas de R$02,00
+    MsgBox ("Pronto! Agora o caixa tem:" & vbCrLf & vnotasnocaixa(0) & " notas de R$100,00" & vbCrLf & vnotasnocaixa(1) & " notas de R$50,00" & vbCrLf & vnotasnocaixa(2) & " notas de R$20,00" & vbCrLf & vnotasnocaixa(3) & " notas de R$10,00" & vbCrLf & vnotasnocaixa(4) & " notas de R$05,00" & vbCrLf & vnotasnocaixa(5) & " notas de R$02,00")
+    Call fmenu
+End Sub
